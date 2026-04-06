4b646df053028fcd215d7acc2ba4931cb2d109eb
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-06 11:59:07 +0600

    Inini: Projects setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navber from '../Components/Navber/Navber';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navber />
+            <Outlet />
+        </div>
+    );
+};
+
+export default MainLayout;
