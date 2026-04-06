4d3370dcf3a1ffe73b7880a33661fba9adae4c01
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-06 12:04:30 +0600

    Added Reacr router routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import MainLayout from './Layout/MainLayout.jsx'
-import Homepage from './Pages/Homepage/Homepage.jsx'
-import Books from './Pages/Books/Books.jsx'
+import { router } from './Routes/Routes'
+import { RouterProvider } from 'react-router'
 
-const router = createBrowserRouter([
-  {path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Homepage />
-
-      },
-      {
-        path: '/books',
-        element: <Books />
-      }
-    ]
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router}/>
+    <RouterProvider router={router }/>
   </StrictMode>,
 )
